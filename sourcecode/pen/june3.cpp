34462ff1acc5b79d491e5b0ec9abdeec8cce2a1f
Matthew James Briggs <matthew.james.briggs@gmail.com>
2018-06-03 22:55:45 -0700

    trim original xml

--- sourcecode/pen/june3.cpp
+++ sourcecode/pen/june3.cpp
@@ -27,6 +27,75 @@ namespace pen
             input.encoding = mx::api::EncodingData{};
             input.partGroups.clear();
             input.pageTextItems.clear();
+            
+            int partIndex = 0;
+            for( auto& part : input.parts )
+            {
+                int measureIndex = 0;
+                for( auto& measure : part.measures )
+                {
+                    if( measureIndex == 0 || measure.staves.at( 0 ).clefs.size() > 0 )
+                    {
+                        mx::api::ClefData clef;
+                        clef.setTreble();
+                        
+                        if( partIndex == 2 )
+                        {
+                            clef.setAlto();
+                        }
+                        
+                        if( partIndex == 3 )
+                        {
+                            clef.setBass();
+                        }
+
+                        if( measure.staves.at( 0 ).clefs.empty() )
+                        {
+                            measure.staves.at( 0 ).clefs.push_back( clef );
+                        }
+                        else
+                        {
+                            measure.staves.at( 0 ).clefs.at( 0 ) = clef;
+                        }
+                    }
+                    
+                    if( measure.staves.size() == 2 )
+                    {
+                        measure.staves.erase( measure.staves.cbegin() + 1 );
+                    }
+                    
+                    for( auto& staff : measure.staves )
+                    {
+                        for( auto& voice : staff.voices )
+                        {
+                            for( auto& note: voice.second.notes )
+                            {
+                                if( note.pitchData.step == mx::api::Step::a )
+                                {
+                                    if( note.pitchData.alter == -1 )
+                                    {
+                                        note.pitchData.step = mx::api::Step::g;
+                                        note.pitchData.alter = 1;
+                                    }
+                                }
+                                if( note.pitchData.alter == -1 )
+                                {
+                                    note.pitchData.accidental = mx::api::Accidental::flat;
+                                }
+                                else if( note.pitchData.alter == 1 )
+                                {
+                                    note.pitchData.accidental = mx::api::Accidental::sharp;
+                                }
+                            }
+                        }
+                    }
+                
+                    ++measureIndex;
+                }
+                
+                ++partIndex;
+            }
+            
             const auto resaveID = dmgr.createFromScore( input );
             dmgr.writeToFile( resaveID, inFilepath );
         }
